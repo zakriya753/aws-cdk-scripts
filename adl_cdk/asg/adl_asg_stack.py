c5c5380a2a28a76d19e85c3c34e74d0eefd4f6a1
zakriya753 <69262326+zakriya753@users.noreply.github.com>
2020-08-06 20:48:01 +0500

    update adl_asg_stack.py

--- adl_cdk/asg/adl_asg_stack.py
+++ adl_cdk/asg/adl_asg_stack.py
@@ -8,8 +8,8 @@ class Adl_Asg_Stack(core.Stack):
     def __init__(self, scope: core.Construct, id: str, **kwargs) -> None:
         super().__init__(scope, id, **kwargs)
 
-    def asg_app(self, vpc):
-        asg_app= asg.AutoScalingGroup(self, "asg-app",
+    def asg_app(self, vpc, asg_name):
+        asg_app= asg.AutoScalingGroup(self, f"asg-app{asg_name}",
             vpc=vpc['vpc'],
             instance_type=ec2.InstanceType.of(ec2.InstanceClass.BURSTABLE2, ec2.InstanceSize.MICRO),
             machine_image=ec2.AmazonLinuxImage(),
